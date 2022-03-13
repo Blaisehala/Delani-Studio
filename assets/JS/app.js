fc0b7665c686ec12021bacc1bdb9a90f52b9067e
Blaise Hala <blaisehala@gmail.com>
2022-03-13 10:47:39 +0300

    feat:adds alert feature

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -20,3 +20,32 @@ $('.management').click(()=>{
 
 
 });
+
+
+
+
+
+
+function message (){
+  var name = document.getElementById('name');
+  var email = document.getElementById('email');
+  var msg = document.getElementById('msg');
+
+
+  if (name.value === "" || msg.value === '') {
+    alert('please leave a message')
+  } else {
+    alert('Thank you for your message')
+  }
+}
+
+
+const form = document.getElementById('sbb');
+
+form.addEventListener('click', function(event){
+
+  event.preventDefault();
+  
+  message();
+  
+  });
